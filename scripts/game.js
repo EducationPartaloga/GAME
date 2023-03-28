89b2930d78d576d6c313397373e229697fa8d998
PartalogaIrina <7718118@gmail.com>
2023-03-28 20:26:23 +0300

    111

---
+++ scripts/game.js
@@ -0,0 +1,57 @@
+import Stars from "./stars.js"
+import MenuScene from "./menu.js"
+import Background from "./background.js"
+
+
+export default class GameScene {
+    constructor(game) {
+
+        this.game = game
+        this.stars = []
+        this.Init()
+    }
+
+    Init() {
+        for (let i = 0; i < 100; i++){
+            this.stars.push(new Stars(Math.random() * this.game.canvas.width,  Math.random() * this.game.canvas.height))
+        }
+        this.background = new Background(this.game)
+    }
+
+    update(dt) {
+
+        this.stars.forEach(
+            (star, index) => {
+
+                if (star.position.y > this.game.canvas.height) {
+                    this.stars.splice(index, 1, new Stars(Math.random() * this.game.canvas.width, -10))
+                }
+
+                star.update(dt)
+            }
+        )
+
+        this.background.update(dt)
+
+        if (this.game.checkKeyPress('Escape')) {
+            this.game.seyScene(MenuScene);
+        }
+        
+    }
+
+    render(dt, ctx, canvas) {
+        ctx.fillStyle = '#000000'
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+        this.background.render(dt, ctx, canvas)
+
+
+        this.stars.forEach(
+            (star, index) => {
+                star.render(dt, ctx, canvas)
+            }
+        )
+
+    }
+
+}
